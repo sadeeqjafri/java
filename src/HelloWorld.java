6c81674d03354703bc094ff5c1af0593cabb0b4f
sadeeqjafri <sadeeqjafri@gmail.com>
2020-10-24 09:30:36 +0530

    Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
     public static void main (String ar[]) {
         System.out.println("This is my first java program");
+        int a = 4;
+        float b = 2.0f;
+        double c = 20.20;
+        char ch = 'a';
+        System.out.println("Interger vairable "+ a);
+        System.out.println("Float varaible "+b);
+        System.out.println("Double variable: "+c);
+        System.out.println("Char vairble: "+ch);
     }
 }
